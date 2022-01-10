ed2ed06af91284c5388247033a9b559fc5bcf8a2
Lucas Garcia <lucas.garcia@zup.com.br>
2022-01-10 19:10:08 -0300

    add scrollIntoView in accordion item
    Signed-off-by: Lucas Garcia <lucas.garcia@zup.com.br>

--- themes/techradar/static/js/components/accordion.js
+++ themes/techradar/static/js/components/accordion.js
@@ -7,14 +7,16 @@
         e.preventDefault();
     
         const accordionItem = $(e.target).parents('.cp-accordion-item');
-    
         if (accordionItem.hasClass('is-active')) {
           accordionItem.removeClass('is-active')
         } else {
           $('.cp-accordion-item').removeClass('is-active')
-    
           accordionItem.addClass('is-active')
         }
+
+        accordionItem[0].scrollIntoView({
+          block: "start", behavior: "smooth"
+        });
       }
     
       accordionToggles.on('click', handleToggleClick)
