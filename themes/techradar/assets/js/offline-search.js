f2b64f9f3111ff4c6df33369bc90562714128372
Darwin Boaventura <darwin.araujo@zup.com.br>
2022-01-18 16:50:45 -0300

    fix: pt-br search
    Signed-off-by: Darwin Boaventura <darwin.araujo@zup.com.br>

--- themes/techradar/assets/js/offline-search.js
+++ themes/techradar/assets/js/offline-search.js
@@ -3,25 +3,37 @@
 ;(function ($) {
   'use strict'
 
+  const removeLastBarFromString = string => {
+    const lastCharIndex = string.length - 1
+
+    if (string[lastCharIndex] === '/') {
+      return string.slice(0, -1)
+    }
+
+    return string
+  }
+
+  const removeSymbolsFromString = (string) => {
+    let wip = string;
+
+    wip = wip.replace('-', '');
+    wip = wip.replace('_', '');
+
+    return wip;
+  };
+
+  const baseURL = removeLastBarFromString($('meta[name=baseURL]').attr('content'))
+
   $(document).ready(function () {
-    const CURRENT_LANGUAGE = $('html').attr('lang')
     const $searchInput = $('.cp-search-bar-field')
 
-    if ($searchInput && $searchInput.length) {
-      //
-      // Options for popover
-      //
+    const currentLanguage = removeSymbolsFromString($('html').attr('lang'))
+    const offlineJsonUri = $searchInput.data('offline-search-index-json-src');
 
+    if ($searchInput && $searchInput.length) {
       $searchInput.data('html', true)
       $searchInput.data('placement', 'bottom')
-      $searchInput.data(
-        'template',
-        '<div class="popover offline-search-result" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
-      )
-
-      //
-      // Register handler
-      //
+      $searchInput.data('template', '<div class="popover offline-search-result" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>')
 
       $searchInput.on('change', event => {
         render($(event.target))
@@ -43,7 +55,7 @@
       const resultDetails = new Map() // Will hold the data for the search results (titles and summaries)
 
       // Set up for an Ajax call to request the JSON data file that is created by Hugo's build process
-      $.ajax($searchInput.data('offline-search-index-json-src')).then(data => {
+      $.ajax(offlineJsonUri).then(data => {
         idx = lunr(function () {
           this.ref('ref')
 
@@ -57,6 +69,8 @@
           this.field('language')
 
           data.forEach(doc => {
+            doc.language = removeSymbolsFromString(doc.language);
+
             this.add(doc)
 
             resultDetails.set(doc.ref, {
@@ -83,8 +97,7 @@
           return
         }
 
-        const results = idx
-          .query(q => {
+        const results = idx.query(q => {
             const tokens = lunr.tokenizer(searchQuery.toLowerCase())
 
             tokens.forEach(token => {
@@ -92,22 +105,23 @@
 
               q.term(queryString, {
                 boost: 100
-              })
+              });
+
               q.term(queryString, {
-                wildcard:
-                  lunr.Query.wildcard.LEADING | lunr.Query.wildcard.TRAILING,
+                wildcard: lunr.Query.wildcard.LEADING | lunr.Query.wildcard.TRAILING,
                 boost: 10
-              })
+              });
+
               q.term(queryString, {
                 editDistance: 2
-              })
+              });
             })
 
-            q.term(`content/${CURRENT_LANGUAGE}`, {
+            q.term(`content/${currentLanguage}`, {
               fields: ['language'],
               wildcard: lunr.Query.wildcard.LEADING | lunr.Query.wildcard.TRAILING,
               presence: lunr.Query.presence.REQUIRED
-            }) 
+            })
           })
           .slice(0, $targetSearchInput.data('offline-search-max-results'))
 
@@ -123,24 +137,15 @@
         $html.append($searchResultBody)
 
         if (results.length === 0) {
-          $searchResultBody.append(
-            $('<p>').text(`No results found for query "${searchQuery}"`)
-          )
+          $searchResultBody.append($('<p>').text(`No results found for query "${searchQuery}"`))
         } else {
           results.forEach((r, index) => {
             if (index <= 2) {
               const doc = resultDetails.get(r.ref)
-              const baseURL = $searchInput.data('offline-search-base-href')
 
-              const $entry = $('<div>').addClass(
-                'row justify-content-between align-items-center cp-search-results-found-item'
-              )
+              const $entry = $('<div>').addClass('row justify-content-between align-items-center cp-search-results-found-item')
 
-              $entry.append(
-                $('<span>')
-                  .addClass('col-auto cp-search-results-found-item-title')
-                  .text(doc.title)
-              )
+              $entry.append($('<span>').addClass('col-auto cp-search-results-found-item-title').text(doc.title))
 
               // $entry.append(
               //   $('<span>')
@@ -148,13 +153,7 @@
               //     .text(doc.excerpt)
               // )
 
-              $entry.append(
-                $('<span>')
-                  .addClass('col-auto')
-                  .append(
-                    `<a href="${r.ref}"><img src="${baseURL}icons/arrow-right.svg" alt="Ir"></a>`
-                  )
-              )
+              $entry.append($('<span>').addClass('col-auto').append(`<a href="${r.ref}"><img src="${baseURL}/icons/arrow-right.svg" alt="Ir"></a>`))
 
               $searchResultBody.append($entry)
             }
--- themes/techradar/static/js/app.js
+++ themes/techradar/static/js/app.js
@@ -1,15 +1,15 @@
 (() => {
-  const removeLastBarFromString = (string) => {
-    const lastCharIndex = string.length - 1;
-
-    if (string[lastCharIndex] === '/') {
-      return string.slice(0, -1);
-    }
-
-    return string;
-  };
-
   $(document).ready(() => {
+    const removeLastBarFromString = (string) => {
+      const lastCharIndex = string.length - 1;
+    
+      if (string[lastCharIndex] === '/') {
+        return string.slice(0, -1);
+      }
+    
+      return string;
+    };
+    
     const baseURL = removeLastBarFromString($("meta[name=baseURL]").attr("content"));
     
     const size = 700;
@@ -317,7 +317,7 @@
       };
 
       blips.click((e) => handler($(e.target).parents('.blip-link')));
-      
+
       sidebarItems.click((e) => {
         e.preventDefault();
         
